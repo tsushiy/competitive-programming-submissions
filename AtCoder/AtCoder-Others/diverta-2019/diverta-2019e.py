d04e0a0396b83293193403145f7624edcb60b7ca
tsushiy <28962399+tsushiy@users.noreply.github.com>
2019-05-13 01:38:40 +0900

    Update

--- AtCoder/AtCoder-Others/diverta-2019/diverta-2019e.py
+++ AtCoder/AtCoder-Others/diverta-2019/diverta-2019e.py
@@ -6,22 +6,20 @@ for i in range(1, n):
   s[i] = s[i-1]^a[i]
 
 MOD = 10**9+7
-dp0 = [1 for i in range(1<<20)]
-dp1 = [0 for i in range(1<<20)]
+dp = [[not j for i in range(1<<20)] for j in range(2)]
 cnt = [0 for i in range(1<<20)]
 z = 0
 for i in range(n):
   if s[i]==0:
     z += 1
-  else:
-    dp0[s[i]] = (dp0[s[i]]+dp1[s[i]]*(z-cnt[s[i]]))%MOD
-    dp1[s[i]] = (dp1[s[i]]+dp0[s[i]])%MOD
-    cnt[s[i]] = z
+  dp[0][s[i]] = (dp[0][s[i]]+dp[1][s[i]]*(z-cnt[s[i]])%MOD)%MOD
+  dp[1][s[i]] = (dp[1][s[i]]+dp[0][s[i]])%MOD
+  cnt[s[i]] = z
 
 if s[n-1]:
-  print(dp0[s[n-1]])
+  print(dp[0][s[n-1]])
 else:
   ans = pow(2, z-1, MOD)
-  for i in range(1<<20):
-    ans += dp1[i]
+  for i in range(1, 1<<20):
+    ans = (ans+dp[1][i])%MOD
   print(ans)
